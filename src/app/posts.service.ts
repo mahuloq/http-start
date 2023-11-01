fa9027c0ca56e182c222f6bf0435941242bd8b0e
Matt Tucker <arcanemahuloq@gmail.com>
2023-10-31 22:06:22 -0500

    Delete posts button

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -40,4 +40,10 @@ export class PostsService {
         })
       );
   }
+
+  deletePosts() {
+    return this.http.delete(
+      "https://http-start-5f6d7-default-rtdb.firebaseio.com/posts.json"
+    );
+  }
 }
