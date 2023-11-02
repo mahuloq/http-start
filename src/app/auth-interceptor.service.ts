a46d4c23cdb1a9123e58c66d33d7de5b6b21a3b4
Matt Tucker <arcanemahuloq@gmail.com>
2023-11-01 21:38:38 -0500

    Saved File

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -1,11 +1,8 @@
 import {
-  HttpEvent,
-  HttpEventType,
   HttpHandler,
   HttpInterceptor,
   HttpRequest,
 } from "@angular/common/http";
-import { tap } from "rxjs/operators";
 
 export class AuthInterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler) {
